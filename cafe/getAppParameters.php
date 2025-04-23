6910762da81d1253b243cb35397e3e20c8bc6ae4
shaikalthaf65 <shaikalthaf3437@gmail.com>
2025-04-23 10:35:38 +0530

    Update getAppParameters.php

--- cafe/getAppParameters.php
+++ cafe/getAppParameters.php
@@ -1,4 +1,5 @@
 
+
 <?php
 
 require 'aws.phar'; // AWSSDK
@@ -23,5 +24,46 @@ try {
   $db_url = $secrets_client->getSecretValue([
     'SecretId' => '/cafe/dbUrl1'
   ]);
-  $db_url = $db_url["
-jqd-pboh-eeu
+  $db_url = $db_url["SecretString"];
+
+  $db_user = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/dbUser1'
+  ]);
+  $db_user = $db_user["SecretString"];
+
+  $db_password = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/dbPassword1'
+  ]);
+  $db_password = $db_password["SecretString"];
+
+  $db_name = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/dbName1'
+  ]);
+  $db_name = $db_name["SecretString"];
+
+  $currency = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/currency1'
+  ]);
+  $currency = $currency["SecretString"];  
+
+  $timezone = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/timeZone1'
+  ]);
+  $timezone = $timezone["SecretString"];  
+
+  $showServerInfo = $secrets_client->getSecretValue([
+    'SecretId' => '/cafe/showServerInfo1'
+  ]);
+  $showServerInfo = $showServerInfo["SecretString"];  
+
+}
+catch (Exception $e) {
+  $db_url = '';
+  $db_name = '';
+  $db_user = '';
+  $db_password = '';
+  $showServerInfo = '';
+  $timeZone = '';
+  $currency = '';
+}
+?>
